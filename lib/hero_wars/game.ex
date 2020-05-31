1b0351fb66ad5c41b69f0a148a465e2e836ef3da
Timotej Fartek <timotej@fartek.si>
2020-05-31 11:34:14 +0200

    Add 'move' and 'attack' functionality to Heroes (#5)

--- lib/hero_wars/game.ex
+++ lib/hero_wars/game.ex
@@ -29,4 +29,84 @@ defmodule HeroWars.Game do
         {:error, :cannot_spawn_hero}
     end
   end
+
+  @spec move_hero(Hero.name(), :up | :down | :left | :right) :: Hero.t()
+  def move_hero(name, :up) do
+    hero = HeroServer.current_state(name)
+    proposed_position = {hero.x_pos, hero.y_pos - 1}
+
+    if World.walkable?(proposed_position) do
+      HeroServer.reposition(name, proposed_position)
+    else
+      hero
+    end
+  end
+
+  def move_hero(name, :down) do
+    hero = HeroServer.current_state(name)
+    proposed_position = {hero.x_pos, hero.y_pos + 1}
+
+    if World.walkable?(proposed_position) do
+      HeroServer.reposition(name, proposed_position)
+    else
+      hero
+    end
+  end
+
+  def move_hero(name, :left) do
+    hero = HeroServer.current_state(name)
+    proposed_position = {hero.x_pos - 1, hero.y_pos}
+
+    if World.walkable?(proposed_position) do
+      HeroServer.reposition(name, proposed_position)
+    else
+      hero
+    end
+  end
+
+  def move_hero(name, :right) do
+    hero = HeroServer.current_state(name)
+    proposed_position = {hero.x_pos + 1, hero.y_pos}
+
+    if World.walkable?(proposed_position) do
+      HeroServer.reposition(name, proposed_position)
+    else
+      hero
+    end
+  end
+
+  @spec attack(Hero.name()) :: :ok
+  def attack(name) do
+    hero = HeroServer.current_state(name)
+    positions_to_attack = calculate_positions_to_attack({hero.x_pos, hero.y_pos})
+    [{hero_pid, _}] = Registry.lookup(:hero_registry, name)
+
+    HeroSupervisor
+    |> Supervisor.which_children()
+    |> Stream.map(fn {_, pid, _, _} -> pid end)
+    |> Stream.filter(fn enemy_pid ->
+      enemy = HeroServer.current_state(enemy_pid)
+      enemy_position = {enemy.x_pos, enemy.y_pos}
+      self? = enemy_pid == hero_pid
+
+      !self? && enemy_position in positions_to_attack
+    end)
+    |> Stream.each(&HeroServer.update_life_status(&1, false))
+    |> Stream.run()
+  end
+
+  @spec calculate_positions_to_attack(Hero.position()) :: [Hero.position()]
+  defp calculate_positions_to_attack({x_pos, y_pos} = _position) do
+    [
+      {x_pos - 1, y_pos - 1},
+      {x_pos, y_pos - 1},
+      {x_pos + 1, y_pos - 1},
+      {x_pos - 1, y_pos},
+      {x_pos, y_pos},
+      {x_pos + 1, y_pos},
+      {x_pos - 1, y_pos + 1},
+      {x_pos, y_pos + 1},
+      {x_pos + 1, y_pos + 1}
+    ]
+  end
 end
